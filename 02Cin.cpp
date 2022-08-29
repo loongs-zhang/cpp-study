23fc9dbe9360451fd8ec276013238f9ad1d224ba
hailang <hailang@webuy.ai>
2022-08-29 19:00:21 +0800

    cin和注释

---
+++ 02Cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/**
+ * 多行注释
+ * @author dragon-zhang
+ * @date 2022/08/29
+ */
+int main() {
+    int v1 = 0, v2 = 0;
+    //单行注释
+    std::cout << "please input two numbers..." << std::endl;
+    std::cin >> v1 >> v2;
+    std::cout << "v1:" << v1 << ",v2:" << v2 << std::endl;
+    return 0;
+}
