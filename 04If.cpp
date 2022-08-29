ba86215e692a5d2194766b4281fc581afadaae90
hailang <hailang@webuy.ai>
2022-08-29 20:30:49 +0800

    if

---
+++ 04If.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 3; ++i) {
+        if (i == 0) {
+            std::cout << "a thousand miles begins with a single step" << std::endl;
+        }
+    }
+    return 0;
+}
